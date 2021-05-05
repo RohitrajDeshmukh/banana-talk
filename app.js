d739163bc892f996a33a5d02b1ddf5ef00d57527
RohitajDeshmukh <74870688+RohitajDsehmukh@users.noreply.github.com>
2021-05-05 21:01:57 +0530

    done

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var output = document.querySelector("#output")
 
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+function getTranslatedURL(text){
+  return serverURL + "?" + "text" + text
+}
+
+function errorHandler(error){
+  console.log("error", error);
+  alert("somthing went wrong with server! plz.. try somtime..")
+}
+
 function clickEventHandler(){
-  output.innerHTML = "bannananananana " + txtInput.value 
-  } 
+  // output.innerHTML = "bannananananana " + txtInput.value 
+  var inputText = txtInput.value;
+
+  fetch(getTranslatedURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated;
+    output.innerHTML = translatedText;
+  })
+  .catch(errorHandler)
+  }; 
 
 
 btnTranslate.addEventListener("click",  clickEventHandler)
